374b3995a53e591584dc9f276f4d5aa3b3c852dc
Jimmy Pan <pantc12@gmail.com>
2018-05-21 18:58:34 +0800

    Make variable name more readable

--- app.py
+++ app.py
@@ -13,36 +13,36 @@ API_TOKEN = config['TRELLO']['API_TOKEN']
 
 def getUser():
     r = requests.get(API_MEMBERS_ME, params={'key': API_KEY, 'token': API_TOKEN})
-    resp = json.loads(r.text)
-    return resp
+    user = json.loads(r.text)
+    return user
 
 def getUserBoards():
     data = []
     r = requests.get(API_MEMBERS_ME_BOARDS, params={'key': API_KEY, 'token': API_TOKEN})
-    resp = json.loads(r.text)
-    for i in resp:
-        data.append({'name': i['name'], 'id': i['id']})
+    boards = json.loads(r.text)
+    for board in boards:
+        data.append({'name': board['name'], 'id': board['id']})
     return data
 
 def getCardsOfUserWithBoardID(userid, boardid):
-    data = []
+    cardsOfUser = []
     r = requests.get(API_BOARDS_CARD.format(id=boardid), params={'key': API_KEY, 'token': API_TOKEN})
-    resp = json.loads(r.text)
-    for i in resp:
-        if userid in i['idMembers']:
-            data.append(i)
-    return data
+    cards = json.loads(r.text)
+    for card in cards:
+        if userid in card['idMembers']:
+            cardsOfUser.append(card)
+    return cardsOfUser
 
 def main():
-    card_count = 0
+    cardOfUserCount = 0
     user = getUser()
     userid = user['id']
     boards = getUserBoards()
     for board in boards:
         count = len(getCardsOfUserWithBoardID(userid, board['id']))
-        card_count += count
+        cardOfUserCount += count
         print("{board}: {count}".format(board=board['name'], count=count))
-    print("Total Cards of {username}: {count}".format(username=user['initials'], count=card_count))
+    print("Total of {username}: {count}".format(username=user['initials'], count=cardOfUserCount))
 
 if __name__ == '__main__':
     main()
